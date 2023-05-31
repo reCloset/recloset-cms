bfdfd64496f964a24686f590f59960a273f6d08e
tau-bar <taufiq.b.ar@gmail.com>
2023-05-31 21:33:04 +0800

    initial

---
+++ index.js
@@ -0,0 +1,10 @@
+const functions = require('@google-cloud/functions-framework');
+
+// Register an HTTP function with the Functions Framework
+functions.http('myHttpFunction', (req, res) => {
+  // Your code here
+
+  // Send an HTTP response
+  res.send('OK');
+});
+    
