367fd56bc91006287724259bb6de39d3b5baaddb
tau-bar <taufiq.b.ar@gmail.com>
2023-06-01 00:18:49 +0800

    add nsfw checker

--- functions/index.js
+++ functions/index.js
@@ -14,13 +14,12 @@ const busboy = require('busboy');
 const os = require('os');
 const path = require('path');
 const fs = require('fs')
+const tf = require('@tensorflow/tfjs-node');
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
 exports.helloWorld = onRequest(async (req, res) => {
-    logger.info("Hello logs!", {structuredData: true});
-
     if (req.method !== 'POST') {
         return res.status(400).json({ error: 'Method not allowed' });
     }
@@ -51,18 +50,30 @@ exports.helloWorld = onRequest(async (req, res) => {
 
     bb.on('finish', () => {
         if (!imageFilePath) {
-            return res.status(400).json({ error: 'No image provided' });
-          }
-        // Upload the temporary file to your desired storage location (e.g., Firebase Storage)
-        // Here you can use the Firebase Admin SDK or any other storage library to perform the upload
-        console.log(imageFilePath)
-        // After the upload is complete, you can perform further processing or store the file path in a database
+            return res.status(400).json({ error: `No image provided: ${err}` });
+        }
 
-        // Send a response indicating the successful upload
-        res.status(200).json({ message: 'Image uploaded successfully' });
+        fs.readFile(imageFilePath, async (err, buffer) => {
+            if (err) {
+                return res.status(500).json({ error: err });
+            }
+
+            const image = await tf.node.decodeImage(buffer, 3);
+
+            const preds = await nsfwjs.load()
+            .then((model) => {
+                return model.classify(image)
+            }).then((predictions) => {
+                image.dispose();
+                return predictions
+            }).catch((err) => {
+                res.status(400).json({ error: `Error while classifying image: ${err}` });
+            });
+        
+            // Send a response indicating the successful upload
+            res.status(200).json(preds);
+        });
     });
 
     bb.end(req.rawBody);
-
-    // res.send("Hello from Firebase!");
 });
